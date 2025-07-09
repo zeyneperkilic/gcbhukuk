1ba5fbd5b9aebe34f0958d3b9156accaa175ed68
zeyneperkilic <zeynepp.erkilic@gmail.com>
2025-07-09 17:30:41 +0300

    Navbar rengi güncellendi ve port 5001'e ayarlandı

--- app.py
+++ app.py
@@ -81,5 +81,9 @@ def giris():
 def kayit():
     return render_template('kayit.html')
 
+@app.route('/yayinlar')
+def yayinlar():
+    return render_template('yayinlar.html')
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
+    app.run(debug=True, host='0.0.0.0', port=5001) 
